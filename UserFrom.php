e8f45e3c46d2ae20f9926e86aadd39213944c4a8
codewithrohit370 <rohitbgmi370@gmail.com>
2026-09-16 01:34:26 +0530

    1.2

--- UserFrom.php
+++ UserFrom.php
@@ -33,9 +33,41 @@
         <button name="enter">Show details</button>
     </form>
 
+
+
     <p>Name : <?php echo $nm ?? ''?></p>
     <p>Email : <?php echo $em ?? ''?></p>
     <p>Age : <?php echo $ag ?? ''?></p>
     <p>FeedBack : <?php echo $feedBack ?? ''?></p>
+
+
+    <?php
+        if(isset($_POST['check'])){
+            $name = $_POST['nm'];
+            $age = $_POST['ag'];
+
+            if($age>=18){
+            $result = $name . " Your Age is ". $age ." So Your eligble For vote";
+        }elseif($age<0){
+            $result = "Invaild Entry";
+        }
+        else{
+            $result = $name . " Your Age is ". $age ." So Your Not eligble For vote";
+        }
+        }
+        
+    ?>
+    <h1 style="text-align:center;">Let's Check You are eligble for Vote or Not</h1>
+    <form  method="post">
+    <label for="">Enter Your Name</label>
+    <input type="text" name="nm">
+    <br>
+    <label for="">Enter Age</label>
+    <input type="number" name=ag>
+
+    <button name='check' class='result'>See Result</button>
+    </form>
+
+    <h3><?php echo $result?></h3>
 </body>
 </html>
